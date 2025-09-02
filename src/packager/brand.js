94a8658a1b8a8be8daf27d49adceabb93e784c3e
DBDev <dambul2809@mail.ru>
2025-09-02 17:25:45 +0000

    Brand corrections

--- src/packager/brand.js
+++ src/packager/brand.js
@@ -31,10 +31,8 @@ If this is set to an empty string, the link is removed.
 
 module.exports = {
   APP_NAME: 'Dash Packager',
-  WEBSITE: 'https://dashblocks.github.io/packager.html',
-  COPYRIGHT_NOTICE: `Copyright (C) 2021-2024 Thomas Weber
-
-The Source Code Form of this program is available under the terms of the
+  WEBSITE: 'https://dashblocks.github.io/packager',
+  COPYRIGHT_NOTICE: `The Source Code Form of this program is available under the terms of the
 Mozilla Public License, v. 2.0. You can obtain a copy of the license at
 https://www.mozilla.org/en-US/MPL/2.0/.`,
   ACCENT_COLOR: '#ffb74cff',
