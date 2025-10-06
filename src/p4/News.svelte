fec15ce4793663e73ea7326832557c85071c62ed
DBDev <dambul2809@mail.ru>
2025-10-06 06:39:51 +0000

    Some rebranding

--- src/p4/News.svelte
+++ src/p4/News.svelte
@@ -22,7 +22,7 @@
       We've updated the packager to use the <a href="https://docs.turbowarp.org/new-compiler">new compiler</a> to make projects run even faster.
     </p>
     <p>
-      If your project relies on extensions that require the old compiler, a version of the packager using the old compiler will be available on <a href="https://packager-legacy.turbowarp.org/">packager-legacy.turbowarp.org</a> indefinitely.
+      If your project relies on extensions that require the old compiler, a version of the packager using the old compiler will be available on <a href="https://dashblocks.github.io/packager-legacy/">dashblocks.github.io/packager-legacy</a> indefinitely.
     </p>
   </div>
 </Section>
